8ddfaea0b7ef926a7e383b4da53821662d709838
Manish Khanchandani <manishkk74@gmail.com>
2017-06-26 22:26:17 -0700

    Basic Structure

---
+++ Templates/lr.dwt.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<!-- TemplateBeginEditable name="doctitle" -->
+<title>Untitled Document</title>
+<!-- TemplateEndEditable -->
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="../lr/css/bootstrap.min.css">
+<script src="../lr/js/bootstrap.min.js"></script>
+<script src="../lr/js/jquery.min.js"></script>
+
+<!-- TemplateBeginEditable name="head" -->
+<!-- TemplateEndEditable -->
+</head>
+
+<body>
+<div class="container">
+	<!-- TemplateBeginEditable name="EditRegion3" -->
+		<div>my basic content</div> 
+	<!-- TemplateEndEditable -->
+</div>
+</body>
+</html>
