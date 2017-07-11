27f34bd505003f074fab8406de6d6d6e6c9fbb18
Manish Khanchandani <manishkk74@gmail.com>
2017-07-11 06:51:58 -0700

    sql

--- db/project2017.sql
+++ db/project2017.sql
@@ -45,11 +45,11 @@ CREATE TABLE `lr_reminders` (
   `status` int(1) NOT NULL DEFAULT '1',
   `reminder_created_dt` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`reminder_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
 /*Data for the table `lr_reminders` */
 
-insert  into `lr_reminders`(`reminder_id`,`user_id`,`title`,`emailTo`,`message`,`fileLink`,`status`,`reminder_created_dt`) values (1,1,'Reminder For My Son','son@mkgalaxy.com','hello son, how r u',NULL,1,'2017-06-30 06:38:35'),(2,1,'Reminder For My Mom','mom@mkgalaxy.com','hello mom, how r u',NULL,1,'2017-06-30 06:39:17'),(3,1,'My Friend','friend1@mkgalaxy.com','hello friend1, how r u','http://google.com',0,'2017-06-30 06:39:32'),(4,2,'For My Friend A','friendA@mkgalaxy.com','hi friend A, how are you',NULL,1,'2017-06-30 06:40:19'),(5,2,'For My Friend B','friendB@mkgalaxy.com','hi friend B, how are you',NULL,1,'2017-06-30 06:40:50');
+insert  into `lr_reminders`(`reminder_id`,`user_id`,`title`,`emailTo`,`message`,`fileLink`,`status`,`reminder_created_dt`) values (1,1,'Reminder For My Son','son@mkgalaxy.com','hello son, how r u',NULL,1,'2017-06-30 06:38:35'),(2,1,'Reminder For My Mom','mom@mkgalaxy.com','hello mom, how r u',NULL,1,'2017-06-30 06:39:17'),(3,1,'My Friend','friend1@mkgalaxy.com','hello friend1, how r u','http://google.com',0,'2017-06-30 06:39:32'),(4,2,'For My Friend A','friendA@mkgalaxy.com','hi friend A, how are you',NULL,1,'2017-06-30 06:40:19'),(5,2,'For My Friend B','friendB@mkgalaxy.com','hi friend B, how are you',NULL,1,'2017-06-30 06:40:50'),(6,1,'Test 1','test@mkgalaxy.com','test','test',1,'2017-07-03 21:59:29');
 
 /*Table structure for table `lr_users` */
 
@@ -73,7 +73,7 @@ CREATE TABLE `lr_users` (
 
 /*Data for the table `lr_users` */
 
-insert  into `lr_users`(`user_id`,`email`,`password`,`first_name`,`last_name`,`gender`,`birth_year`,`created_dt`,`login_dt`,`emailFlag1`,`emailFlag1Date`,`cronFlag`) values (1,'manny@mkgalaxy.com','password','Manish','Khanchandani','male',1974,'2017-06-30 06:35:03',1498886393,0,NULL,0),(2,'carrie@mkgalaxy.com','password','Carrie','P','female',1970,'2017-06-30 06:35:03',1498797303,0,NULL,0),(3,'kate@mkgalaxy.com','password','Kate','L','female',1986,'2017-06-30 06:35:03',1498797303,0,NULL,0);
+insert  into `lr_users`(`user_id`,`email`,`password`,`first_name`,`last_name`,`gender`,`birth_year`,`created_dt`,`login_dt`,`emailFlag1`,`emailFlag1Date`,`cronFlag`) values (1,'manny@mkgalaxy.com','password','Manish','Khanchandani','male',1974,'2017-06-30 06:35:03',1391369252,1,1499317108,1),(2,'carrie@mkgalaxy.com','password','Carrie','P','female',1970,'2017-06-30 06:35:03',1291369252,1,1499317108,1),(3,'kate@mkgalaxy.com','password','Kate','L','female',1986,'2017-06-30 06:35:03',1191369252,1,1499317108,1);
 
 /*Table structure for table `qz_categories` */
 
@@ -85,11 +85,11 @@ CREATE TABLE `qz_categories` (
   `parent_id` int(11) DEFAULT '0',
   `user_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`cat_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
 
 /*Data for the table `qz_categories` */
 
-insert  into `qz_categories`(`cat_id`,`category`,`parent_id`,`user_id`) values (1,'Law',0,1),(2,'First Year',1,1),(3,'Contracts',2,1),(4,'Criminal Law',2,1),(5,'Torts Law',2,1),(6,'Web Development',0,1),(7,'Javascript',6,1),(8,'Legal Writings',2,1),(9,'Set 1',3,1);
+insert  into `qz_categories`(`cat_id`,`category`,`parent_id`,`user_id`) values (1,'Law',0,1),(2,'First Year',1,1),(3,'Contracts',2,1),(4,'Criminal Law',2,1),(5,'Torts Law',2,1),(6,'Web Development',0,1),(7,'Javascript',6,1),(8,'Legal Writings',2,1),(9,'Set 1',3,1),(13,'Definition 1',3,1);
 
 /*Table structure for table `qz_questions` */
 
@@ -128,9 +128,11 @@ CREATE TABLE `qz_results` (
   `wrong_results` int(11) DEFAULT NULL,
   `calc_percentage` double DEFAULT NULL,
   PRIMARY KEY (`cid`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 CHECKSUM=1 DELAY_KEY_WRITE=1 ROW_FORMAT=DYNAMIC;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 CHECKSUM=1 DELAY_KEY_WRITE=1 ROW_FORMAT=DYNAMIC;
 
 /*Data for the table `qz_results` */
+
+insert  into `qz_results`(`cid`,`category_id`,`user_id`,`total_question`,`correct_results`,`cdate`,`results`,`wrong_results`,`calc_percentage`) values (8,9,1,3,3,'2017-07-02 18:34:47','{\"1\":\"3\",\"2\":\"3\",\"3\":\"3\"}',0,100);
 
 /*Table structure for table `qz_users` */
 
