4156936b705915b96152d73978338ce2963aa94a
Manish Khanchandani <manishkk74@gmail.com>
2017-07-13 22:32:53 -0700

    df

--- Templates/lr.dwt.php
+++ Templates/lr.dwt.php
@@ -27,30 +27,38 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">Project name</a>
+          <a class="navbar-brand" href="http://lifereminder.tk">LifeReminder.tk</a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
           <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
+            <li class="active"><a href="../lr/index.php">Home</a></li>
+            <li><a href="../lr/faq.php">FAQ</a></li>
+            <li><a href="../lr/our_team.php">Our Team</a></li>
+            <li><a href="../lr/about.php">About</a></li>
+            <li><a href="../lr/contact.php">Contact</a></li>
             <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Reminders <span class="caret"></span></a>
               <ul class="dropdown-menu">
-                <li><a href="#">Action</a></li>
-                <li><a href="#">Another action</a></li>
-                <li><a href="#">Something else here</a></li>
-                <li role="separator" class="divider"></li>
-                <li class="dropdown-header">Nav header</li>
-                <li><a href="#">Separated link</a></li>
-                <li><a href="#">One more separated link</a></li>
+                <li><a href="../lr/reminder/new_reminder.php">Create New Reminder</a></li>
+                <li><a href="../lr/reminder/list_reminders.php">My Reminders</a></li>
               </ul>
             </li>
           </ul>
           <ul class="nav navbar-nav navbar-right">
-            <li><a href="../navbar/">Default</a></li>
-            <li class="active"><a href="./">Static top <span class="sr-only">(current)</span></a></li>
-            <li><a href="../navbar-fixed-top/">Fixed top</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Users <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+			  	<?php if (empty($_SESSION['MM_UserId'])) { ?>
+                <li><a href="../lr/users/register.php">Register New User</a></li>
+                <li><a href="../lr/users/login.php#">Login</a></li>
+                <li><a href="../lr/users/forgot.php#">Forgot Password</a></li>
+				<?php } ?>
+				<?php if (!empty($_SESSION['MM_UserId'])) { ?>
+                <li><a href="../lr/users/change_password.php">Change Password</a></li>
+                <li><a href="../lr/users/logout.php">Logout</a></li>
+				<?php } ?>
+              </ul>
+            </li>
           </ul>
         </div><!--/.nav-collapse -->
       </div>
