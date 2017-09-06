952463150951511cd73d42f057159a220deddde3
Manish Khanchandani <manishkk74@gmail.com>
2017-09-06 06:56:53 -0700

    s

--- Connections/conn.php
+++ Connections/conn.php
@@ -3,8 +3,8 @@
 # Type="MYSQL"
 # HTTP="true"
 $hostname_conn = "remote-mysql4.servage.net";
-$database_conn = "alaw";
-$username_conn = "alaw";
-$password_conn = "manishkk74";
+$database_conn = "";
+$username_conn = "";
+$password_conn = "";
 $conn = mysql_connect($hostname_conn, $username_conn, $password_conn) or trigger_error(mysql_error(),E_USER_ERROR); 
 ?>
