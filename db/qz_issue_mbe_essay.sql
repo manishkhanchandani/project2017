a3c5f8841781fbc982208d504d2ed86cae56c481
Manish Khanchandani <manishk@chegg.com>
2017-10-06 11:43:58 -0700

    more

--- db/qz_issue_mbe_essay.sql
+++ db/qz_issue_mbe_essay.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Oct 05, 2017 at 06:24 PM
+-- Generation Time: Oct 06, 2017 at 07:37 PM
 -- Server version: 10.1.25-MariaDB
 -- PHP Version: 5.6.31
 
