a7c012502e0fb64276001fb602b923f7d5e59109
Manish Khanchandani <manishk@chegg.com>
2017-10-17 04:58:36 -0700

    final sql

--- db/qz_categories.sql
+++ db/qz_categories.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Oct 13, 2017 at 05:06 PM
+-- Generation Time: Oct 17, 2017 at 01:56 PM
 -- Server version: 10.1.25-MariaDB
 -- PHP Version: 5.6.31
 
--- db/qz_questions.sql
+++ db/qz_questions.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Oct 17, 2017 at 01:54 AM
+-- Generation Time: Oct 17, 2017 at 01:55 PM
 -- Server version: 10.1.25-MariaDB
 -- PHP Version: 5.6.31
 
