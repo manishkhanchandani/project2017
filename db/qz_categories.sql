78bb451a190d996ccd4f6878cc3dd16f48e98358
Manish Khanchandani <manishk@chegg.com>
2017-10-22 19:08:21 -0700

    more

--- db/qz_categories.sql
+++ db/qz_categories.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Oct 19, 2017 at 01:17 PM
+-- Generation Time: Oct 23, 2017 at 04:04 AM
 -- Server version: 10.1.25-MariaDB
 -- PHP Version: 5.6.31
 
