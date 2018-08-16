f98a5933e05374e1eadf6300731ef310e5e97b75
Manish Khanchandani <manishkk74@gmail.com>
2018-08-16 16:46:06 -0700

    s

--- combo/sites/d/NavMulti.php
+++ combo/sites/d/NavMulti.php
@@ -11,7 +11,7 @@
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
-				<a class="navbar-brand" href="<?php echo HTTP_PATH; ?>">INeedService.US</a>
+				<a class="navbar-brand" href="<?php echo HTTP_PATH; ?>">Donation</a>
 			</div>
 			<div class="collapse navbar-collapse">
 				<ul class="nav navbar-nav navbar-right">
---
+++ combo/sites/g/manager/create.php
@@ -0,0 +1,1 @@
+<?php include(COMMON_CODE.'/manager/create.php'); ?>
