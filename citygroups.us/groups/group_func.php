30d00f05c4b77578b1a08069a6ddb2d9f698e77e
Manish Khanchandani <manishkk74@gmail.com>
2018-10-29 10:15:52 -0700

    f

---
+++ citygroups.us/groups/group_func.php
@@ -0,0 +1,68 @@
+<?php
+
+class Group {
+
+	public function __construct() {
+	
+	}
+
+	public function createNewGroup($location, $lat=null, $lng=null) {
+		$return = array();
+		$return['url'] = HTTP_PATH;
+		$return['totalRows_rsGroups'] = -1;
+		if (empty(location)) {
+			return $return;
+		}
+
+		$colname_rs = (get_magic_quotes_gpc()) ? $location : addslashes($location);
+		$query = sprintf("select * from citygroup_groups WHERE group_name LIKE '%%%s%%'", $colname_rs);
+		mysql_select_db($database_conn, $conn);
+		$rs = mysql_query($query);
+		if (mysql_num_rows($rs) === 0) {
+			
+			if (empty($lat)) {
+				$return['totalRows_rsGroups'] = 0;
+				return $return;
+			}
+			
+			
+				$group_name = $_POST['location'];
+				$_POST['group_description'] = '';
+				$uid = !empty($_SESSION['MM_UserId']) ? $_SESSION['MM_UserId'] : 0;
+				$insertSQL = sprintf("INSERT INTO citygroup_groups (group_name, group_description, country, `state`, county, city, addr, lat, lng, group_creator_id, ip_address) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
+							   GetSQLValueString($_POST['location'], "text"),
+							   GetSQLValueString($_POST['group_description'], "text"),
+							   GetSQLValueString($_POST['country'], "text"),
+							   GetSQLValueString($_POST['state'], "text"),
+							   GetSQLValueString($_POST['county'], "text"),
+							   GetSQLValueString($_POST['city'], "text"),
+							   GetSQLValueString($_POST['addr'], "text"),
+							   GetSQLValueString($_POST['lat'], "double"),
+							   GetSQLValueString($_POST['lng'], "double"),
+							   GetSQLValueString($uid, "int"),
+							   GetSQLValueString($_SERVER['REMOTE_ADDR'], "text"));
+				  mysql_select_db($database_conn, $conn);
+				  $Result1 = mysql_query($insertSQL, $conn) or die(mysql_error());
+				  $id = mysql_insert_id();
+			
+		} else {
+			$rec = mysql_fetch_array($rs);
+			$id = $rec['group_id'];
+			$group_name = $rec['group_name'];
+		}
+	
+	
+		if (!empty($id)) {
+			$url = HTTP_PATH."groups/?group_id=".$id."&city=".urlencode($group_name);
+			
+			mysql_select_db($database_conn, $conn);
+			$query_rsGroups = "SELECT * FROM citygroup_groups WHERE group_id = $id";
+			$rsGroups = mysql_query($query_rsGroups, $conn) or die(mysql_error());
+			$row_rsGroups = mysql_fetch_assoc($rsGroups);
+			$totalRows_rsGroups = mysql_num_rows($rsGroups);
+		}
+
+	}
+}
+
+$group = new Group();
